ecccf13889313f025cecccf8ac39985a359cc5b3
kruegerc <58778238+kruegerc@users.noreply.github.com>
2026-08-02 21:13:21 +0200

    Add unknown trigger option

--- MigraLog/Models/HeadacheOptions.swift
+++ MigraLog/Models/HeadacheOptions.swift
@@ -4,5 +4,5 @@ enum HeadacheOptions {
     static let painTypes = ["Pulsierend", "Stechend", "Einseitig links", "Einseitig rechts", "Drückend", "Beidseitig"]
     static let locations = ["Stirn", "Schläfe", "Hinterkopf", "Augen", "Nacken"]
     static let symptoms = ["Übelkeit", "Lichtempfindlichkeit", "Lärmempfindlichkeit", "Aura", "Schwindel"]
-    static let triggers = ["Stress", "Schlafmangel", "Wetter", "Ernährung", "Zyklus", "Bildschirmzeit"]
+    static let triggers = ["Unbekannt", "Stress", "Schlafmangel", "Wetter", "Ernährung", "Zyklus", "Bildschirmzeit"]
 }
