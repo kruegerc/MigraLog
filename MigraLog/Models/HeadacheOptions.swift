38863747ee09b343d91545801d90046c6a4f8e68
kruegerc <58778238+kruegerc@users.noreply.github.com>
2026-08-02 20:44:47 +0200

    Pair one-sided pain type options

--- MigraLog/Models/HeadacheOptions.swift
+++ MigraLog/Models/HeadacheOptions.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum HeadacheOptions {
-    static let painTypes = ["Pulsierend", "Stechend", "Drueckend", "Einseitig links", "Einseitig rechts", "Beidseitig"]
+    static let painTypes = ["Pulsierend", "Stechend", "Einseitig links", "Einseitig rechts", "Drueckend", "Beidseitig"]
     static let locations = ["Stirn", "Schlaefe", "Hinterkopf", "Augen", "Nacken"]
     static let symptoms = ["Uebelkeit", "Lichtempfindlichkeit", "Laermempfindlichkeit", "Aura", "Schwindel"]
     static let triggers = ["Stress", "Schlafmangel", "Wetter", "Ernaehrung", "Zyklus", "Bildschirmzeit"]
